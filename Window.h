d0fd39f0b8c38fb2d5f5e2c7277df6e02c01c452
Diego Contreras <dieg7074@gmail.com>
2025-10-25 10:05:55 -0600

    Add files via upload

---
+++ Window.h
@@ -0,0 +1,50 @@
+#pragma once
+#include<stdio.h>
+#include<glew.h>
+#include<glfw3.h>
+
+class Window
+{
+public:
+	Window();
+	Window(GLint windowWidth, GLint windowHeight);
+	int Initialise();
+	GLfloat getBufferWidth() { return bufferWidth; }
+	GLfloat getBufferHeight() { return bufferHeight; }
+	GLfloat getXChange();
+	GLfloat getYChange();
+	GLfloat getmuevex() { return muevex; }
+	GLboolean getprendida() { return luzprendida; }
+	GLboolean getprendidaD() { return luzfaroD; }
+	GLboolean getprendidaT() { return luzfaroT; }
+	GLboolean getprendidaA() { return luzfaroA; }
+	GLfloat getarticulacion3() { return articulacion3; }
+	GLfloat getarticulacion4() { return articulacion4; }
+
+	bool getShouldClose() {
+		return  glfwWindowShouldClose(mainWindow);
+	}
+	bool* getsKeys() { return keys; }
+	void swapBuffers() { return glfwSwapBuffers(mainWindow); }
+
+	~Window();
+private:
+	GLFWwindow* mainWindow;
+	GLint width, height;
+	bool keys[1024];
+	GLint bufferWidth, bufferHeight;
+	void createCallbacks();
+	GLfloat lastX;
+	GLfloat lastY;
+	GLfloat xChange;
+	GLfloat yChange;
+	GLfloat muevex;
+	GLfloat articulacion3, articulacion4;
+	GLboolean luzprendida, luzfaroD, luzfaroT, luzfaroA;
+	bool mouseFirstMoved;
+	static void ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode);
+	static void ManejaMouse(GLFWwindow* window, double xPos, double yPos);
+
+};
+
+
